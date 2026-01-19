45e308ac9c477c3b159a1beb97328f172b524311
lakshyamodi832 <lakshyaymodi@gmail.com>
2026-01-20 01:33:42 +0530

    area

---
+++ area.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+    double side, len, width, rad;
+
+    printf("Enter the side of the square: ");
+    scanf("%lf", &side);
+
+    printf("Area of the square: %lf\n", side*side);
+    printf("Perimeter of the square: %lf\n", 4*side);
+
+    printf("Enter the length & width of the rectangle: ");
+    scanf("%lf %lf", &len, &width);
+
+    printf("Area of the rectangle: %lf\n", len*width);
+    printf("Perimeter of the rectangle: %lf\n", 2*(len+width));
+
+    printf("Enter the radius of the square: ");
+    scanf("%lf", &rad);
+
+    printf("Area of the circle: %lf\n", 3.14*rad*rad);
+    printf("Circumference of the circle: %lf\n", 2*3.14*rad);
+    
+    return 0;
+}
